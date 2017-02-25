6352223d732de2bbb7aef98126110b49ea840d3b
turboc1208 <chip.cox@coxdogs.com>
2017-02-25 15:50:01 -0600

    implemented my_appapi

--- doors.py
+++ doors.py
@@ -1,7 +1,6 @@
-import appdaemon.appapi as appapi
-from utils import *
+import my_appapi as appapi
              
-class doors(appapi.AppDaemon):
+class doors(appapi.my_appapi):
 
   def initialize(self):
     # self.LOGLEVEL="DEBUG"
@@ -18,7 +17,6 @@ class doors(appapi.AppDaemon):
     action_list={}
     entityin=None
     if entity_id in self.door_list:
-      #action_list=self.build_light_list(self.door_list[entity_id])
       action_list["entity_id"]=self.door_list[entity_id]
       self.log("action_list={}".format(action_list))
       for e in action_list:
@@ -32,36 +30,15 @@ class doors(appapi.AppDaemon):
     else:
       self.log("{} not found in {}".format(entity_id,self.door_list))
 
-  def build_light_list(self,entityin):
-    elist=[]
-    for object in self.get_state(entityin,attribute='all')["attributes"]["entity_id"]:
-      device, entity = self.split_entity(object)
-      if device=="group":
-        # if the device is a group recurse back into this function to process the group.
-        elist.update(self.build_light_list(object))
-      else:
-        elist.append({"entity_id":object, "device":device,"entity":entity})
-    self.log("elist={}".format(elist),level="INFO")
-    return(elist)
-
-  def log(self,msg,level="INFO"):
-    obj,fname, line, func, context, index=inspect.getouterframes(inspect.currentframe())[1]
-    super(doors,self).log("{} - ({}) {}".format(func,str(line),msg),level)
-
-def set_house_state(self,entity,state):
-    if self.entity_exists(entity):
-      self.select_option(entity,state)
-      retval=self.get_state(entity)
-    else:
-      retval=None
-    return(retval)
-
-def get_house_state(self,entity):
-    if self.entity_exists(entity):
-      state=self.get_state(entity)
-      self.log("house state={}".format(state),"DEBUG")
-    else:
-      state=None
-    return(state)
-
+#  def build_light_list(self,entityin):
+#    elist=[]
+#    for object in self.get_state(entityin,attribute='all')["attributes"]["entity_id"]:
+#      device, entity = self.split_entity(object)
+#      if device=="group":
+#        # if the device is a group recurse back into this function to process the group.
+#        elist.update(self.build_light_list(object))
+#      else:
+#        elist.append({"entity_id":object, "device":device,"entity":entity})
+#    self.log("elist={}".format(elist),level="INFO")
+#    return(elist)
 
